35935f01b63cc98761a18f5a6b2a4d98486d7dff
Dominique Luna <dluna132@gmail.com>
2017-05-03 21:04:27 -0700

    add further explanation pertaining to Lf

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -9,7 +9,16 @@ using CppAD::AD;
 size_t N = 0;
 double dt = 0;
 
-// NOTE: DON'T CHANGE THIS IT WAS CAREFULLY CHOSEN!!!
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
 class FG_eval {
