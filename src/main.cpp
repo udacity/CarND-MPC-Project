234982f31be5b340243bf74b09dde570bf3ea722
Dominique Luna <dluna132@gmail.com>
2017-05-08 00:10:02 -0700

    Update main.cpp
    add a reminder to set latency to 100ms before submission.

--- src/main.cpp
+++ src/main.cpp
@@ -110,6 +110,9 @@ int main() {
           //
           // Feel free to play around with this value but should be to drive
           // around the track with 100ms latency.
+          //
+          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
+          // SUBMITTING.
           this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
