c8a63d30f5aa768d1040991193d7c3cb6a680bdf
awbrown90 <aaronwbrown@msn.com>
2017-05-15 19:42:19 -0700

    feat: Adding the ability to send the simulator information about MPC path points and reference line points from the script that are then displayed in the simulator. This can be a great debugging tool, and gives good insight to how the MPC is behaving.

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,28 @@ int main() {
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
+
+          //Display the MPC predicted trajectory 
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+
+          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+          // the points in the simulator are connected by a Green line
+
+          msgJson["mpc_x"] = mpc_x_vals;
+          msgJson["mpc_y"] = mpc_y_vals;
+
+          //Display the waypoints/reference line
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+          // the points in the simulator are connected by a Yellow line
+
+          msgJson["next_x"] = next_x_vals;
+          msgJson["next_y"] = next_y_vals;
+
+
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           // Latency
