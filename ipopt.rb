05b4312963d17b594a60c766fdf9d1010cc9ad29
Michael Virgo <mvirgs44@gmail.com>
2018-02-09 11:23:11 -0800

    add ipopt install script

---
+++ ipopt.rb
@@ -0,0 +1,64 @@
+# Script obtained from the https://github.com/Homebrew/homebrew-science repository
+
+class Ipopt < Formula
+  desc "Large-scale nonlinear optimization package"
+  homepage "https://projects.coin-or.org/Ipopt"
+  url "https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.8.tgz"
+  sha256 "62c6de314220851b8f4d6898b9ae8cf0a8f1e96b68429be1161f8550bb7ddb03"
+  revision 1
+  head "https://projects.coin-or.org/svn/Ipopt/trunk", :using => :svn
+
+  bottle :disable, "needs to be rebuilt with latest open-mpi"
+
+  option "without-test", "Skip build-time tests (not recommended)"
+  deprecated_option "without-check" => "without-test"
+
+  depends_on "ampl-mp" => :recommended
+  depends_on "openblas" => :optional
+  depends_on "pkg-config" => :build
+
+  # IPOPT is not able to use parallel MUMPS.
+  depends_on "mumps" => ["without-mpi"] + ((build.with? "openblas") ? ["with-openblas"] : [])
+
+  depends_on :fortran
+
+  def install
+    ENV.delete("MPICC")  # configure will pick these up and use them to link
+    ENV.delete("MPIFC")  # which leads to the linker crashing.
+    ENV.delete("MPICXX")
+    mumps_libs = %w[-ldmumps -lmumps_common -lpord -lmpiseq]
+    mumps_incdir = Formula["mumps"].opt_libexec/"include"
+    mumps_libcmd = "-L#{Formula["mumps"].opt_lib} " + mumps_libs.join(" ")
+
+    args = ["--disable-debug",
+            "--disable-dependency-tracking",
+            "--prefix=#{prefix}",
+            "--with-mumps-incdir=#{mumps_incdir}",
+            "--with-mumps-lib=#{mumps_libcmd}",
+            "--enable-shared",
+            "--enable-static"]
+
+    if build.with? "openblas"
+      args << "--with-blas-incdir=#{Formula["openblas"].opt_include}"
+      args << "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
+      args << "--with-lapack-incdir=#{Formula["openblas"].opt_include}"
+      args << "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
+    end
+
+    if build.with? "ampl-mp"
+      args << "--with-asl-incdir=#{Formula["ampl-mp"].opt_include}/asl"
+      args << "--with-asl-lib=-L#{Formula["ampl-mp"].opt_lib} -lasl"
+    end
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize # Needs a serialized install
+    system "make", "test" if build.with? "test"
+    system "make", "install"
+  end
+
+  test do
+    # IPOPT still fails to converge on the Waechter-Biegler problem?!?!
+    system "#{bin}/ipopt", "#{Formula["ampl-mp"].opt_pkgshare}/example/wb" if build.with? "ampl-mp"
+  end
+end
