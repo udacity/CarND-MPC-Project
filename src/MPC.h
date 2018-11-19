c93b73626dc5af4ccc34c4924dd3d63f68c25f6e
Michael Virgo <michael.v@udacity.com>
2018-11-19 15:35:12 -0800

    fix: remove using namespace std bug
    Removed `using namespace std` from MPC.h to fix the bug mentioned in #26

--- src/MPC.h
+++ src/MPC.h
@@ -4,8 +4,6 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
-using namespace std;
-
 class MPC {
  public:
   MPC();
@@ -14,7 +12,7 @@ class MPC {
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuations.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  std::vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
